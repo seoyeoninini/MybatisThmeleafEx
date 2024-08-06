40df83554b67b0c3fa98d9a793e1912691876d38
전서연 <syjeon802@gmail.com>
2024-08-06 16:08:40 +0900

    s

---
+++ MybatisThmeleafEx/src/main/java/com/sp/app/controller/BoardController.java
@@ -0,0 +1,199 @@
+﻿package com.sp.app.controller;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.sp.app.common.MyUtil;
+import com.sp.app.domain.Board;
+import com.sp.app.service.BoardService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/bbs/*")
+public class BoardController {
+	private final BoardService service;
+	private final MyUtil myUtil;
+
+	// 스프링 부트 3.x_그래들 에서는 @RequestParam 에서 name(또는 value) 속성을 생략하면 
+	//     Name for argument of type [java.lang.String] not specified, and parameter name information not found in class file either.
+	//     에러가 발생할수 있음
+	// 메소드에서 @RequestMapping(value = "list") 처럼 매핑하면 경고 발생
+	
+	@RequestMapping(value = "list", method = {RequestMethod.GET, RequestMethod.POST})
+	public String list(@RequestParam(name = "page", defaultValue = "1") int current_page,
+			@RequestParam(name = "schType", defaultValue = "all") String schType,
+			@RequestParam(name = "kwd", defaultValue = "") String kwd,
+			HttpServletRequest req,
+			Model model) throws Exception {
+
+		int size = 10; // 한 화면에 보여주는 게시물 수
+		int total_page = 0;
+		int dataCount = 0;
+
+		if (req.getMethod().equalsIgnoreCase("GET")) { // GET 방식인 경우
+			kwd = URLDecoder.decode(kwd, "utf-8");
+		}
+
+		// 전체 페이지 수
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("schType", schType);
+		map.put("kwd", kwd);
+
+		dataCount = service.dataCount(map);
+		if (dataCount != 0) {
+			total_page = myUtil.pageCount(dataCount, size);
+		}
+
+		// 다른 사람이 자료를 삭제하여 전체 페이지수가 변화 된 경우
+		if (total_page < current_page) {
+			current_page = total_page;
+		}
+
+		// 리스트에 출력할 데이터를 가져오기
+		int offset = (current_page - 1) * size;
+		if(offset < 0) offset = 0;
+
+		map.put("offset", offset);
+		map.put("size", size);
+
+		List<Board> list = service.listBoard(map);
+
+		model.addAttribute("list", list);
+		model.addAttribute("page", current_page);
+		model.addAttribute("total_page", total_page);
+		model.addAttribute("dataCount", dataCount);
+		model.addAttribute("size", size);
+
+		model.addAttribute("schType", schType);
+		model.addAttribute("kwd", kwd);
+
+		return "bbs/list";
+	}
+
+	@GetMapping("write")
+	public String writeForm(Model model) throws Exception {
+		model.addAttribute("mode", "write");
+		return "bbs/write";
+	}
+
+	@PostMapping("write")
+	public String writeSubmit(Board dto, HttpServletRequest req) throws Exception {
+		try {
+			dto.setIpAddr(req.getRemoteAddr());
+			service.insertBoard(dto);
+		} catch (Exception e) {
+		}
+
+		return "redirect:/bbs/list";
+	}
+
+	@GetMapping("article")
+	public String article(@RequestParam(name = "num") long num,
+			@RequestParam(name = "page") String page,
+			@RequestParam(name = "schType", defaultValue = "all") String schType,
+			@RequestParam(name = "kwd", defaultValue = "") String kwd,
+			Model model) throws Exception {
+
+		kwd = URLDecoder.decode(kwd, "utf-8");
+
+		String query = "page=" + page;
+		if (kwd.length() != 0) {
+			query += "&schType=" + schType + "&kwd=" + URLEncoder.encode(kwd, "UTF-8");
+		}
+
+		// 조회수 증가 및 해당 레코드 가져 오기
+		service.updateHitCount(num);
+
+		Board dto = service.findById(num);
+		if (dto == null) {
+			return "redirect:/bbs/list?" + query;
+		}
+
+		// 스타일로 처리하는 경우 : style="white-space:pre;"
+		dto.setContent(dto.getContent().replaceAll("\n", "<br>"));
+
+		// 이전 글, 다음 글
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("schType", schType);
+		map.put("kwd", kwd);
+		map.put("num", num);
+
+		Board prevDto = service.findByPrev(map);
+		Board nextDto = service.findByNext(map);
+
+		model.addAttribute("dto", dto);
+		model.addAttribute("prevDto", prevDto);
+		model.addAttribute("nextDto", nextDto);
+
+		model.addAttribute("page", page);
+		model.addAttribute("schType", schType);
+		model.addAttribute("kwd", kwd);
+		model.addAttribute("query", query);
+
+		return "bbs/article";
+	}
+
+	@GetMapping("delete")
+	public String delete(@RequestParam(name = "num") long num,
+			@RequestParam(name = "page") String page,
+			@RequestParam(name = "schType", defaultValue = "all") String schType,
+			@RequestParam(name = "kwd", defaultValue = "") String kwd) throws Exception {
+
+		kwd = URLDecoder.decode(kwd, "utf-8");
+		String query = "page=" + page;
+		if (kwd.length() != 0) {
+			query += "&schType=" + schType + "&kwd=" + URLEncoder.encode(kwd, "UTF-8");
+		}
+
+		try {
+			// 자료 삭제
+			service.deleteBoard(num);
+		} catch (Exception e) {
+		}
+
+		return "redirect:/bbs/list?" + query;
+	}
+
+	@GetMapping("update")
+	public String updateForm(@RequestParam(name = "num") long num,
+			@RequestParam(name = "page") String page,
+			Model model) throws Exception {
+
+		Board dto = service.findById(num);
+		if (dto == null) {
+			return "redirect:/bbs/list?page=" + page;
+		}
+
+		model.addAttribute("mode", "update");
+		model.addAttribute("page", page);
+		model.addAttribute("dto", dto);
+
+		return "bbs/write";
+	}
+
+	@PostMapping("update")
+	public String updateSubmit(Board dto,
+			@RequestParam(name = "page") String page) throws Exception {
+		try {
+			// 수정 하기
+			service.updateBoard(dto);
+		} catch (Exception e) {
+		}
+
+		return "redirect:/bbs/list?page=" + page;
+	}
+}
